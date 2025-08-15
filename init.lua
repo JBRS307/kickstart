22da9b3df93fd2ea68c53ced0fb4b3635fd8f540
Jakub Rękas <jakub.rekas@swmansion.com>
2025-08-15 14:49:23 +0200

    Added search in current directory.

--- init.lua
+++ init.lua
@@ -465,6 +465,9 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+      vim.keymap.set('n', '<leader>scd', function()
+        builtin.find_files { cwd = vim.fn.expand '%:p:h' }
+      end, { desc = '[S]earch only in [C]urrent [D]irectory' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
       -- Slightly advanced example of overriding default behavior and theme
@@ -724,6 +727,8 @@ require('lazy').setup({
         eslint = {},
         --
         bashls = {},
+        cssls = {},
+        tailwindcss = {},
 
         lua_ls = {
           -- cmd = { ... },
