a12c5553e7f4ff4b719d131306e227f71f8d59ec
Jakub Rękas <kuba.rekas.307@gmail.com>
2025-10-20 07:52:14 +0200

    ts_ls and eslint added

--- init.lua
+++ init.lua
@@ -736,6 +736,8 @@ require('lazy').setup({
         },
         html = {},
         gh_actions_ls = {},
+        ts_ls = {},
+        eslint = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
