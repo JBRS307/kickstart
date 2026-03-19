435cfd657de8b962f49f38d055aa9d237b5da6eb
Jakub Rękas <kuba.rekas.307@gmail.com>
2026-03-19 14:14:13 +0100

    New telescope keybinds

--- init.lua
+++ init.lua
@@ -411,11 +411,11 @@ require('lazy').setup({
         --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
         --   },
         -- },
-        pickers = {
-          find_files = {
-            hidden = true,
-          },
-        },
+        -- pickers = {
+        --   find_files = {
+        --     hidden = true,
+        --   },
+        -- },
         extensions = {
           ['ui-select'] = { require('telescope.themes').get_dropdown() },
         },
@@ -430,6 +430,12 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set(
+        'n',
+        '<leader>sF',
+        function() builtin.find_files { no_ignore = true, hidden = true } end,
+        { desc = '[S]earch [F]iles (include hidden and ignored)' }
+      )
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set({ 'n', 'v' }, '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
@@ -437,6 +443,12 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader>scd', function() builtin.find_files { cwd = vim.fn.expand '%:p:h' } end, { desc = '[S]earch only in [C]urrent [D]irectory' })
+      vim.keymap.set(
+        'n',
+        '<leader>scD',
+        function() builtin.find_files { cwd = vim.fn.expand '%:p:h', hidden = true, no_ignore = true } end,
+        { desc = '[S]earch only in [C]urrent [D]irectory (include hidden and ignored)' }
+      )
       vim.keymap.set('n', '<leader>scc', builtin.commands, { desc = '[S]earch [C]ommands' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
