f21abe99c88f88d2bd6df5b6dd7a92a03151aaf5
Jakub Rękas <kuba.rekas.307@gmail.com>
2026-04-17 13:32:11 +0200

    Pylsp annoying line length lint disabled

--- init.lua
+++ init.lua
@@ -692,7 +692,17 @@ require('lazy').setup({
         jsonls = {},
         elixirls = {},
         docker_language_server = {},
-        pylsp = {},
+        pylsp = {
+          settings = {
+            pylsp = {
+              plugins = {
+                pycodestyle = {
+                  ignore = { 'E501' },
+                },
+              },
+            },
+          },
+        },
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
